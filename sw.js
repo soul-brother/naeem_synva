cedf05f026d5f37a6d4448b3075d03d6d8faf753
soul-brother <jonade.naeem@gmail.com>
2021-01-19 15:00:09 +0100

    update8

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ self.addEventListener("fetch", fetchEvent => {
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
-      .register("/sw.js")
+      .register("/naeem_synva/sw.js")
       .then(res => console.log("service worker registered"))
       .catch(err => console.log("service worker not registered", err))
   })
