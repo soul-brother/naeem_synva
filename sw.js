6175819dade5fdb08e1f1683cd8c54558ad9ed02
soul-brother <jonade.naeem@gmail.com>
2021-01-19 14:07:59 +0100

    december19

---
+++ sw.js
@@ -0,0 +1,33 @@
+const synvaChat = "synvaChat"
+const assets = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/script.js",
+  "/catmail.png",
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })
+}
